155a02a3738698abe512d6ce105d13d46788d87b
Code_Turtle24 <132008528+FocusLamp@users.noreply.github.com>
2024-05-06 17:54:20 +0300

    Linked List time: O(n) space: O(n)

---
+++ Medium/2487. Remove Nodes From Linked List.py
@@ -0,0 +1,28 @@
+        # Medium Difficulty O(n) O(n)
+
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current = head
+        stack = []
+        
+        while current:
+            while stack and stack[-1].val < current.val:
+                stack.pop()
+            stack.append(current)
+            current = current.next
+        
+        previous_node = None
+        while stack:
+            current = stack.pop()
+            current.next = previous_node
+            previous_node = current
+        
+        return current
